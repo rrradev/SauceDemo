ce3d5aae03cd9084fa31ccf436a7584a65167146
Radoslav Radev <rrradev@users.noreply.github.com>
2022-07-11 20:38:42 +0300

    CI setup

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,13 +1,9 @@
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.Suite;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
-import static io.cucumber.junit.platform.engine.Constants.*;
-
-@Suite
-@IncludeEngines("cucumber")
-@ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/java/features")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, steps.config.GlobalHooks," +
-        " html:target/reports/cucumber-reports.html")
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/features",
+        plugin = {"pretty", "steps.config.GlobalHooks", "html:target/reports/cucumber-reports.html"})
 public class TestRunner {
 }
