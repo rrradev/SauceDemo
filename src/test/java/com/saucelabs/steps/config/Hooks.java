947ea23e2e9172e85a546d1a3c8cd27bc7f1dd56
radradef <radradef@gmail.com>
2024-09-11 12:13:45 +0300

    make feature files run in parallel

--- src/test/java/com/saucelabs/steps/config/Hooks.java
+++ src/test/java/com/saucelabs/steps/config/Hooks.java
@@ -2,16 +2,26 @@ package com.saucelabs.steps.config;
 
 import io.cucumber.java8.LambdaGlue;
 import io.cucumber.java8.Scenario;
+import lombok.SneakyThrows;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks extends BaseSteps implements LambdaGlue {
 
     public Hooks() {
-        After((Scenario scenario) -> {
-            byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-            scenario.attach(screenshot, "image/png", "Screenshot");
+        Before((Scenario scenario) -> {
+            setupDriver();
         });
 
+        After((Scenario scenario) -> {
+            tryToTakeScreenshot(scenario);
+            quitDriver();
+        });
+    }
+
+    @SneakyThrows
+    private void tryToTakeScreenshot(Scenario scenario) {
+        byte[] screenshot = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.BYTES);
+        scenario.attach(screenshot, "image/png", "Screenshot");
     }
 }
